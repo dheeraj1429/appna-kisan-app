cc8f0b741b3802ee81b6e038e72bfbc0f48b1934
dheeraj8585 <dheeraj@metaversitytechnologies.com>
2023-11-26 16:56:07 +0530

    admin can approve user accounts. and download user documents

--- ssa_admin/src/api/b2bApproval.js
+++ ssa_admin/src/api/b2bApproval.js
@@ -1,8 +1,16 @@
 import { axiosInstance } from 'src/services/axios.service'
 
-export const getAllB2bApprovalAccounts = async function() {
-    const response = await axiosInstance.get('/fact')
+export const getAllB2bApprovalAccounts = async function({page}) {
+    const response = await axiosInstance.get(`/api/get-all-b2b-users?page=${page}`)
     return response
 }
 
-export const getSingleB2bAccountDetails = async function() {}
+export const getSingleB2bAccountDetails = async function({userId}) {
+    const response = await axiosInstance.get(`/api/get-single-b2b-user/${userId}`)
+    return response
+}
+
+export const approveUserInformation = async function({userId, is_approved}) {
+    const response = await axiosInstance.patch(`/api/b2b/user/${userId}/approval`, {is_approved})
+    return response
+}
--- ssa_admin/src/services/axios.service.js
+++ ssa_admin/src/services/axios.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://catfact.ninja'
+    baseURL: process.env.REACT_APP_BACKEND_URL
 })
 
 axios.interceptors.request.use(function (config) {
