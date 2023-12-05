740215334a411aa6debf38cc2f01013ca0f73f92
dheeraj8585 <dheeraj@metaversitytechnologies.com>
2023-12-05 17:15:30 +0530

    removing native code

--- ssa_app_react_native/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    primaryColor:'#1BAC4B', // #f40009 green color
-    animationColor:'#5ecb89',
-    BACKEND_URI:'https://ssa-backend-xp96y.ondigitalocean.app',  //   https://ssa-backend-xp96y.ondigitalocean.app 'http://192.168.0.103:5000 '
-    EMAILJS_SERVICE_ID:'service_r3e5wpa',
-    EMAILJS_TEMPLATE_ID:'template_ral5vl8',
-    EMAILJS_PUBLIC_KEY:'B1z1ueDowk1uytEMB'
-
-}
